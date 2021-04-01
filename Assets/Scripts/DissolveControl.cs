0865e2114746c66e4443b188809123bdf51b89d5
Igor Doslov <s191719@students.aie.edu.au>
2021-04-01 15:57:47 +1100

    restart

---
+++ Assets/Scripts/DissolveControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveControl : MonoBehaviour
+{
+    MeshRenderer mRend;
+    public bool shouldDissolve;
+    public bool shouldUndissolve;
+    [Range(0, 1)] public float myVal = 0f;
+    public float dissolveTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRend = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldDissolve)
+        {
+            StartDissolve();
+        }
+    }
+
+    public void StartDissolve()
+    {
+        myVal += dissolveTime * Time.deltaTime;
+        mRend.material.SetFloat("_Amount", myVal);
+        Invoke("SetInactive", 2.6f);
+    }
+
+    public void Undissolve()
+    {
+        myVal -= dissolveTime * Time.deltaTime;
+        mRend.material.SetFloat("_Amount", myVal);
+        Invoke("SetAactive", 2.6f);
+    }
+
+    public void SetInactive()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SetActive()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/RayViewer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayViewer : MonoBehaviour
+{
+
+    public float weaponRange = 200.0f;
+    private Camera fpsCam;
+    RaycastHit hit;
+    public GameObject target;
+    public float lerpSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fpsCam = GetComponentInParent<ThirdPersonCharacterController>().transform.gameObject.GetComponentInChildren<Camera>();
+        fpsCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 lastPos = target.transform.position;
+
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        Vector3 lineOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        Debug.DrawRay(lineOrigin, fpsCam.transform.forward * weaponRange, Color.green);
+
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+        { target.transform.position = Vector3.Lerp(lastPos, hit.point, lerpSpeed * Time.fixedDeltaTime); }
+        else
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit, weaponRange);
+            target.transform.position = Vector3.Lerp(lastPos, ray.GetPoint(weaponRange), lerpSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
