b9435fbaef820d128536a6c41559e8ec78074402
Igor Doslov <s191719@students.aie.edu.au>
2021-04-07 00:23:22 +1000

    After change

---
+++ Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    Renderer mRend;
+    public Material myMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeMat()
+    {
+        mRend.material = myMat;
+    }
+}
